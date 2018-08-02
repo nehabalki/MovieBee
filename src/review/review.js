f6e682aa26c98145dcdaba9d5904efb8ed6dd97f
Chary Vielma <chary.vielma@yahoo.com>
2018-08-01 23:32:39 -0700

    Update review.js

--- src/review/review.js
+++ src/review/review.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './review.css';
 import firebase, { auth, provider } from '../header/firebase';
+import StarRatingComponent from 'react-star-rating-component';
 
 export class Review extends React.Component {
 
   constructor() {
   super();
   this.state = {
+    rating: 5,
     currentItem: '',
     username: '',
     items: [],
@@ -14,6 +16,10 @@ export class Review extends React.Component {
   }
   this.handleChange = this.handleChange.bind(this);
   this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  onStarClick(nextValue, prevValue, name) {
+    this.setState({rating: nextValue});
   }
 
   handleChange(e) {
@@ -66,6 +72,8 @@ export class Review extends React.Component {
 
   render() {
     const link = 'https://image.tmdb.org/t/p/w300';
+    const { rating } = this.state;
+
     return(
       <div>
         <h3>Review</h3>
@@ -74,22 +82,40 @@ export class Review extends React.Component {
               <form onSubmit={this.handleSubmit}>
                 <input type="text" name="username" placeholder="Username" onChange={this.handleChange} value={this.state.username} />
                 <input type="text" name="currentItem" placeholder="Movie review" onChange={this.handleChange} value={this.state.currentItem} />
+                
+                <div style={{textAlign:"left", fontWeight: "bold"}} >
+                <span>Rating:&nbsp;&nbsp;
+                <StarRatingComponent
+                name="rate1"
+                starCount={10}
+                value={rating}
+                onStarClick={this.onStarClick.bind(this)}/></span>
+                </div>
+
                 <button>Add Review</button>
               </form>
           </section>
+
           <section className='display-item'>
             <div className="wrapper">
               <ul>
               {this.state.items.map((item) => {
-              return (
+              return (    
+                <div>
               <li key={item.id}>
                  <h3>
+                 <span>
+                 <StarRatingComponent
+                 name="rate1"
+                 starCount={10}
+                 value={rating}/></span><br />
                  <span>{item.title} </span>
                  <span id="span2">Reviewed By: {item.user}&nbsp;&nbsp;
                   {item.user === this.state.user.displayName || item.user === this.state.user.email ?
                    <button onClick={() => this.removeItem(item.id)}>Remove Review</button> : null}</span></h3>
-              </li>
-              )
+                   </li>
+                   </div>
+                  )
               })}
               </ul>
             </div>
